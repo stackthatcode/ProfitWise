7da17c7039f6212ede16f12e694ece0da9a109c1
Aleks Jones <aleksjones@gmail.com>
2016-08-19 17:38:43 -0500

    Final refinements on Bulk Editor pop-up.

--- ProfitWise.Web/Scripts/ProfitWiseFunctions.js
+++ ProfitWise.Web/Scripts/ProfitWiseFunctions.js
@@ -59,4 +59,22 @@ ProfitWiseFunctions.PopOverCloseAll = function() {
         });
 };
 
+ProfitWiseFunctions.CurrencyCache = [
+    { Id: 1, Abbr: "USD", Symbol: "$" },
+    { Id: 2, Abbr: "EUR", Symbol: "€" },
+    { Id: 3, Abbr: "JPY", Symbol: "¥" },
+    { Id: 4, Abbr: "GBP", Symbol: "£" },
+    { Id: 5, Abbr: "AUD", Symbol: "$" },
+    { Id: 6, Abbr: "CHF", Symbol: "Fr" },
+    { Id: 7, Abbr: "CAD", Symbol: "$" },
+];
+
+
+ProfitWiseFunctions.CurrencyFormatter = function (amount, currencyId) {
+    var item =
+        AQ(ProfitWiseFunctions.CurrencyCache)
+            .first(function (item) { return item.Id == currencyId; });
+
+    return item.Symbol + numeral(amount).format("0,0.00") + " " + item.Abbr;
+};
 
