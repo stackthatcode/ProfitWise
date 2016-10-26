4eeb6e9dd0058843387bf69515c1fd444eb975f2
Aleks Jones <aleksjones@gmail.com>
2016-10-26 18:29:23 -0500

    Work-in-progress on Report Editor

---
+++ ProfitWise.Web/Controllers/ReportServiceController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using ProfitWise.Data.Factories;
+using ProfitWise.Data.Services;
+using ProfitWise.Web.Attributes;
+
+namespace ProfitWise.Web.Controllers
+{
+    [Authorize(Roles = "ADMIN, USER")]
+    [IdentityProcessor]
+    public class ReportServiceController : Controller
+    {
+        private readonly MultitenantFactory _factory;
+
+        public ReportServiceController(MultitenantFactory factory, CurrencyService currencyService)
+        {
+            _factory = factory;
+        }
+
+
+    }
+}
