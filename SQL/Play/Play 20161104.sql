e5f84d05b4c43730c3c0afe633002ce7f5d36e1c
Aleks <aleksjones@gmail.com>
2016-11-08 16:07:42 -0600

    Work-in-progress on revamp of selection model on reports

--- SQL/Play/Play 20161104.sql
+++ SQL/Play/Play 20161104.sql
@@ -9,7 +9,6 @@ VALUES
 ( 1, 'Test TESt', 0, 0, 0, 0, 0, 'TEST TEST', '2016-01-01', '2016-01-01' );
 
 
-SELECT * FROM profitwisereport;
 
 UPDATE profitwisereport SET Saved = 1;
 
@@ -27,9 +26,52 @@ SELECT ProductType, COUNT(*) FROM profitwiseproduct
 WHERE IsPrimary = 1 GROUP BY ProductType;
 
 
-SELECT * FROM profitwisereportproducttype;
+
+
+SELECT * FROM profitwisereport;
+
+SELECT * FROM profitwisereportproducttype LIMIT 50000;
 
 SELECT * FROM profitwisereportvendor;
+
+
+
+SELECT * FROM profitwisereportproducttype;
+
+
+DROP PROCEDURE TestDataPopulation; 
+
+
+DELIMITER $$
+	CREATE PROCEDURE TestDataPopulation(p1 INT)
+	BEGIN
+		SET autocommit = 0;
+		SELECT 'Ok...';
+
+		label1: LOOP
+			SET p1 = p1 + 1;
+			INSERT INTO profitwisereportproducttype (PwReportId, PwShopId, ProductType) VALUES (99739, 1, p1);
+			
+			IF p1 < 30000 THEN
+			  ITERATE label1;	/* This is the same as C# 'goto' */
+			END IF;
+			
+			LEAVE label1; /* This is the same is C# 'break' */
+			
+		END LOOP label1;
+		
+		COMMIT;
+		SELECT 'Whuttt?';
+		SET @x = p1;		
+	END $$
+DELIMITER ;
+
+
+
+
+CALL TestDataPopulation(1);
+
+
 
 
 SELECT t1.PwMasterProductId, t1.Title, COUNT(*) AS Count
@@ -50,5 +92,6 @@ SELECT * FROM profitwisevariant;
 
 SELECT * FROM profitwisereportmasterproduct;
 
+SELECT * FROM profitwisereportsku;
 
 
