2b3b1e1e2ea60146f6d86339ef74b468c39042ec
Jeremy Simon <jeremy@3duniverse.org>
2016-11-14 15:19:59 -0600

    Added comments to ProfitWise App Table Script and System Tables Scrip

--- SQL/Environment Setup Scripts/ProfitWise App Table Script - v0.2.sql
+++ SQL/Environment Setup Scripts/ProfitWise App Table Script - v0.2.sql
@@ -1,6 +1,6 @@
-USE profitwise;
+USE profitwise; # Set active database
 
-SET SQL_SAFE_UPDATES = 0;
+SET SQL_SAFE_UPDATES = 0; # Turn off Safe Updates setting
 
 
 DROP TABLE IF EXISTS `profitwiseshop`; 
@@ -25,85 +25,86 @@ DROP TABLE IF EXISTS `profitwisepicklistmasterproduct`;
 
 
 CREATE TABLE `profitwiseshop` (
-  `PwShopId` BIGINT NOT NULL AUTO_INCREMENT,
-  `ShopOwnerUserId` varchar(128) NOT NULL,
-  `ShopifyShopId` BIGINT NULL,
-  `CurrencyId` INT NULL,
-  `StartingDateForOrders` TIMESTAMP NULL,
-  `TimeZone` varchar(50) NULL,
+  `PwShopId` BIGINT NOT NULL AUTO_INCREMENT, # ProfitWise's unique identifier for each shop
+  `ShopOwnerUserId` varchar(128) NOT NULL, # Email address of shop owner
+  `ShopifyShopId` BIGINT NULL, # Shopify's internal identifier for each shop
+  `CurrencyId` INT NULL, # Numeric value representing shop currency
+  `StartingDateForOrders` TIMESTAMP NULL, # Starting date for order data to import
+  `TimeZone` varchar(50) NULL, # Shop time zone
   
-  `IsAccessTokenValid` TINYINT NOT NULL,
-  `IsShopEnabled` TINYINT NOT NULL,
-  `IsDataLoaded` TINYINT NOT NULL,
+  `IsAccessTokenValid` TINYINT NOT NULL, # Is the shop's access token currently valid?
+  `IsShopEnabled` TINYINT NOT NULL, # Is the shop currently enabled in ProfitWise?
+  `IsDataLoaded` TINYINT NOT NULL, # Has the shop's data been loaded into ProfitWise?
   
   PRIMARY KEY  (`PwShopId`, `ShopOwnerUserId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100001 DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `profitwisemasterproduct` (
-  `PwMasterProductId` BIGINT NOT NULL AUTO_INCREMENT,
-  `PwShopId` BIGINT NOT NULL,
+  `PwMasterProductId` BIGINT NOT NULL AUTO_INCREMENT, # ProfitWise's unique identifier for each product (can contain multiple "linked" Shopify products)
+  `PwShopId` BIGINT NOT NULL, # ProfitWise's shop identifier
   PRIMARY KEY (`PwMasterProductId`, `PwShopId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `profitwiseproduct` (
-  `PwProductId` BIGINT NOT NULL AUTO_INCREMENT,
-  `PwShopId` BIGINT NOT NULL,
-  `PwMasterProductId` BIGINT NOT NULL,
-  `ShopifyProductId` BIGINT NULL,
+  `PwProductId` BIGINT NOT NULL AUTO_INCREMENT, # ProfitWise's unique identifier for each product (maps to a PwMasterProductId)
+  `PwShopId` BIGINT NOT NULL, # ProfitWise's shop identifier
+  `PwMasterProductId` BIGINT NOT NULL, # ProfitWise's unique identifier for each product (can contain multiple "linked" Shopify products)
+  `ShopifyProductId` BIGINT NULL, # Shopify's product identifier (maps to a PwProductId)
   
-  `Title` VARCHAR(200) DEFAULT NULL,
-  `Vendor` VARCHAR(100) DEFAULT NULL,
-  `ProductType` VARCHAR(100) DEFAULT NULL,
-  `Tags` TEXT DEFAULT NULL,
+  `Title` VARCHAR(200) DEFAULT NULL, # Product title
+  `Vendor` VARCHAR(100) DEFAULT NULL, # Product vendor
+  `ProductType` VARCHAR(100) DEFAULT NULL, # Product type
+  `Tags` TEXT DEFAULT NULL, # Product tags
   
-  `IsActive` TINYINT unsigned NOT NULL,
-  `IsPrimary` TINYINT unsigned NOT NULL,
-  `IsPrimaryManual` TINYINT unsigned NOT NULL,
-  `LastUpdated` TIMESTAMP NOT NULL,
+  `IsActive` TINYINT unsigned NOT NULL, # Is product active in Shopify catalog?
+  `IsPrimary` TINYINT unsigned NOT NULL, # Is this the primary product under the PwMasterProductId?
+  `IsPrimaryManual` TINYINT unsigned NOT NULL, # Was this product manually set as the primary product under the PwMasterProductId?
+  `LastUpdated` TIMESTAMP NOT NULL, # When was this product last updated in Shopify?
   PRIMARY KEY (`PwProductId`, `PwShopId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `profitwisemastervariant` (
-  `PwMasterVariantId` BIGINT NOT NULL AUTO_INCREMENT,
-  `PwShopId` BIGINT NOT NULL,
-  `PwMasterProductId` BIGINT NOT NULL,
-  `Exclude` TINYINT NOT NULL,
-  `StockedDirectly` TINYINT NOT NULL,
-  `CogsCurrencyId` INT NULL,
-  `CogsAmount` DECIMAL(15, 2) NULL,
-  `CogsDetail` TINYINT NULL,
+  `PwMasterVariantId` BIGINT NOT NULL AUTO_INCREMENT, # ProfitWise's unique identifier for each variant (may contain multiple "linked" variants)
+  `PwShopId` BIGINT NOT NULL, # ProfitWise's shop identifier
+  `PwMasterProductId` BIGINT NOT NULL, # ProfitWise's unique identifier for each product (can contain multiple "linked" Shopify products)
+  `Exclude` TINYINT NOT NULL, # Should this variant be excluded from ProfitWise reporting?
+  `StockedDirectly` TINYINT NOT NULL, # Is this variant stocked directly (as opposed to drop-shipped)?
+  `CogsCurrencyId` INT NULL, # Numeric value representing the currency for the CoGS data for this variant
+  `CogsAmount` DECIMAL(15, 2) NULL, # CoGS value for this variant
+  `CogsDetail` TINYINT NULL, # Is there detailed CoGS data (weighted average entries) for this variant?
   PRIMARY KEY (`PwMasterVariantId`,`PwShopId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `profitwisevariant` (
-  `PwVariantId` BIGINT NOT NULL AUTO_INCREMENT,	/** PK **/  
-  `PwShopId` BIGINT NOT NULL,		/** PK **/
-  `PwProductId` BIGINT NOT NULL,	/** Immutable **/  
-  `PwMasterVariantId` BIGINT NOT NULL,		/** Can change i.e. can be assigned to another Master record **/
+  `PwVariantId` BIGINT NOT NULL AUTO_INCREMENT,	# ProfitWise's unique identifier for each variant (maps to a PwMasterVariantId)
+  `PwShopId` BIGINT NOT NULL, # ProfitWise's shop identifier
+  `PwProductId` BIGINT NOT NULL, # ProfitWise's unique identifier for each product (maps to a PwMasterProductId)
+  `PwMasterVariantId` BIGINT NOT NULL, # ProfitWise's unique identifier for each variant (may contain multiple "linked" variants)
+  /** Can change i.e. can be assigned to another Master record **/
   
-  `ShopifyProductId` BIGINT NULL,
-  `ShopifyVariantId` BIGINT NULL,
-  `Sku` VARCHAR(100) NULL,
-  `Title` VARCHAR(200) NULL,
-  `LowPrice` DECIMAL(15, 2) NOT NULL,
-  `HighPrice` DECIMAL(15, 2) NOT NULL,
-  `Inventory` INT NULL,
+  `ShopifyProductId` BIGINT NULL, # Shopify's product identifier (maps to a PwProductId)
+  `ShopifyVariantId` BIGINT NULL, # Shopify's variant identifier (maps to a PwVariantId)
+  `Sku` VARCHAR(100) NULL, # Variant SKU
+  `Title` VARCHAR(200) NULL, # Variant title
+  `LowPrice` DECIMAL(15, 2) NOT NULL, # Lowest price for this variant from order history
+  `HighPrice` DECIMAL(15, 2) NOT NULL, # Highest price for this variant from order history
+  `Inventory` INT NULL, # Inventory count for this variant
   
-  `IsActive` TINYINT NOT NULL,
-  `IsPrimary` TINYINT NOT NULL,
-  `IsPrimaryManual` TINYINT unsigned NOT NULL,
-  `LastUpdated` TIMESTAMP NOT NULL,
+  `IsActive` TINYINT NOT NULL, # Is this variant active in Shopify's catalog?
+  `IsPrimary` TINYINT NOT NULL, # Is this the primary variant under the PwMasterVariantId?
+  `IsPrimaryManual` TINYINT unsigned NOT NULL, # Was this variant manually selected as the parimary variant under the PwMasterVariantId?
+  `LastUpdated` TIMESTAMP NOT NULL, # When was this variant last updated in Shopify?
 
   PRIMARY KEY (`PwVariantId`,`PwShopId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `profitwisebatchstate` (
-  `PwShopId` BIGINT unsigned NOT NULL,
-  `ProductsLastUpdated` TIMESTAMP NULL,
-  `OrderDatasetStart` TIMESTAMP NULL,
-  `OrderDatasetEnd` TIMESTAMP NULL,
+  `PwShopId` BIGINT unsigned NOT NULL, # ProfitWise's shop identifier
+  `ProductsLastUpdated` TIMESTAMP NULL, # When did ProfitWise last update products from Shopify's catalog?
+  `OrderDatasetStart` TIMESTAMP NULL, # Start date for the current order data set to be loaded from Shopify
+  `OrderDatasetEnd` TIMESTAMP NULL, # End date for the current order data set to be loaded from Shopify
    PRIMARY KEY (`PwShopId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -112,34 +113,34 @@ CREATE TABLE `profitwisebatchstate` (
 
 
 CREATE TABLE `shopifyorder` (
-  `PwShopId` int(6) unsigned NOT NULL,
-  `ShopifyOrderId` BIGINT NOT NULL,
-  `Email` varchar(128) DEFAULT NULL,
-  `OrderNumber` varchar(128) DEFAULT NULL,  
-  `OrderLevelDiscount` decimal(15,2) DEFAULT NULL,
-  `SubTotal` decimal(15,2) DEFAULT NULL,
-  `TotalRefund` decimal(15,2) DEFAULT NULL,	
-  `TaxRefundAmount` decimal(15,2) DEFAULT NULL,
-  `ShippingRefundAmount` decimal(15,2) DEFAULT NULL,
-  `FinancialStatus` varchar(25) DEFAULT NULL,
-  `Tags` varchar(500) DEFAULT NULL,  
-  `CreatedAt` timestamp NOT NULL,
-  `UpdatedAt` timestamp NOT NULL,
+  `PwShopId` int(6) unsigned NOT NULL, # ProfitWise's shop identifier
+  `ShopifyOrderId` BIGINT NOT NULL, # Shopify's unique order identifier
+  `Email` varchar(128) DEFAULT NULL, # Email address associated with the order
+  `OrderNumber` varchar(128) DEFAULT NULL, # Shopify order number
+  `OrderLevelDiscount` decimal(15,2) DEFAULT NULL, # Discount applied at the order-level
+  `SubTotal` decimal(15,2) DEFAULT NULL, # Order subtotal
+  `TotalRefund` decimal(15,2) DEFAULT NULL,	# Total of refunds issued for this order
+  `TaxRefundAmount` decimal(15,2) DEFAULT NULL, # Total of taxes refunded for this order
+  `ShippingRefundAmount` decimal(15,2) DEFAULT NULL, # Total of shipping fees refunded for this order
+  `FinancialStatus` varchar(25) DEFAULT NULL, # Financial settlement status of this order
+  `Tags` varchar(500) DEFAULT NULL, # Tags for this order
+  `CreatedAt` timestamp NOT NULL, # Date-time when this order was created
+  `UpdatedAt` timestamp NOT NULL, # Date-time when this order was last updated in Shopify
   PRIMARY KEY  (`PwShopId`, `ShopifyOrderId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `shopifyorderlineitem` (
-  `PwShopId` int(6) unsigned NOT NULL,
-  `ShopifyOrderId` BIGINT NOT NULL,
-  `ShopifyOrderLineId` BIGINT NOT NULL,
-  `OrderDate` timestamp NOT NULL,  
-  `PwProductId` BIGINT NOT NULL, 
-  `PwVariantId` BIGINT NOT NULL,      
-  `Quantity` int(6) unsigned NOT NULL,
-  `UnitPrice` decimal(15,2) DEFAULT NULL,   
-  `TotalDiscount` decimal(15,2) DEFAULT NULL,
-  `TotalRestockedQuantity` int(6) unsigned NOT NULL,
-  `GrossRevenue` decimal(15,2) DEFAULT NULL,
+  `PwShopId` int(6) unsigned NOT NULL, # ProfitWise's shop identifier
+  `ShopifyOrderId` BIGINT NOT NULL, # Shopify identifier for associated order
+  `ShopifyOrderLineId` BIGINT NOT NULL, # Shopify identifier for order line item
+  `OrderDate` timestamp NOT NULL, # Date-time for associated order
+  `PwProductId` BIGINT NOT NULL, # Product (PwProductId) for this line item
+  `PwVariantId` BIGINT NOT NULL, # Variant (PwVariantId) for this line item
+  `Quantity` int(6) unsigned NOT NULL, # Quantity for this line item
+  `UnitPrice` decimal(15,2) DEFAULT NULL, # Unit price for this line item (for qty 1)
+  `TotalDiscount` decimal(15,2) DEFAULT NULL, # Total discount applied to this line item
+  `TotalRestockedQuantity` int(6) unsigned NOT NULL, # Total quantity restocked from this line item
+  `GrossRevenue` decimal(15,2) DEFAULT NULL, # Gross revenue for this line item (after discounts and refunds)
   PRIMARY KEY  (`PwShopId`, `ShopifyOrderId`, `ShopifyOrderLineId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -147,16 +148,16 @@ CREATE TABLE `shopifyorderlineitem` (
 
 
 CREATE TABLE `profitwisepicklist` (
-  `PwPickListId` BIGINT NOT NULL AUTO_INCREMENT,
-  `PwShopId` BIGINT NOT NULL,
-  `CreatedDate` TIMESTAMP NOT NULL,
+  `PwPickListId` BIGINT NOT NULL AUTO_INCREMENT, # Identifier for the picklist containing a list of products to display
+  `PwShopId` BIGINT NOT NULL, # ProfitWise's shop identifier
+  `CreatedDate` TIMESTAMP NOT NULL, # Date-time when this picklist was created
   PRIMARY KEY (`PwPickListId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100001 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `profitwisepicklistmasterproduct` (
-	`PwPickListId` BIGINT NOT NULL,
-    `PwShopId` BIGINT NOT NULL,
-    `PwMasterProductId` BIGINT NOT NULL,
+	`PwPickListId` BIGINT NOT NULL, # Identifier for the picklist containing a list of products to display
+    `PwShopId` BIGINT NOT NULL, # ProfitWise's shop identifier
+    `PwMasterProductId` BIGINT NOT NULL, # Product (PwMasterProductId) to include in this picklist
     PRIMARY KEY (`PwPickListId`, `PwShopId`, `PwMasterProductId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
