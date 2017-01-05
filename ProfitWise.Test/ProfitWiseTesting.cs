83959c822ae2f43c86e92c6692e7ac159dab8973
Jeremy Simon <jeremy@3duniverse.org>
2017-01-05 15:54:16 -0600

    More tests added, with assertions for data validation

--- ProfitWise.Test/ProfitWiseTesting.cs
+++ ProfitWise.Test/ProfitWiseTesting.cs
@@ -69,20 +69,30 @@ namespace ProfitWise.Test
             wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
 
             //Wait for "Select Report" drop-down to be available
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"report-viewer-header\"]/div/div/div[2]/div[1]/a")));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.PartialLinkText("Select Report")));
 
-            driverGC.FindElement(By.XPath("//*[@id=\"report-viewer-header\"]/div/div/div[2]/div[1]/a")).Click();
-            
+            driverGC.FindElement(By.PartialLinkText("Select Report")).Click();
+
             //Wait for Reports drop-down list to be available
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"report-viewer-header\"]/div/div/div[2]/div[1]/ul/li[4]/a/span[2]")));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.PartialLinkText("Overall Profitability 2016")));
 
-            driverGC.FindElement(By.XPath("//*[@id=\"report-viewer-header\"]/div/div/div[2]/div[1]/ul/li[4]/a/span[2]")).Click();
+            driverGC.FindElement(By.PartialLinkText("Overall Profitability 2016")).Click();
 
             //Wait for Spinner Layer to go away
             wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
 
             //Wait for chart to finish loading
             Thread.Sleep(2500);
+
+            //Check 2016 Revenues
+            var Total2016Revenues = driverGC.FindElement(By.XPath("//*[@id=\"report-viewer-body\"]/div/div/div/div[2]/div/div[1]/div[2]/h1")).Text;
+            Assert.AreEqual("$2,656,800.96", Total2016Revenues);
+            Console.WriteLine("Total Revenues for 2016 = " + Total2016Revenues);
+
+            //Check 2016 Profits
+            var Total2016Profit = driverGC.FindElement(By.XPath("//*[@id=\"report-viewer-body\"]/div/div/div/div[2]/div/div[1]/div[4]/h1")).Text;
+            Assert.AreEqual("$584,770.58", Total2016Profit);
+            Console.WriteLine("Total Profit for 2016 = " + Total2016Profit);
 
             Screenshot ss = ((ITakesScreenshot)driverGC).GetScreenshot();
             ss.SaveAsFile("c:\\Screenshots\\OverallProfitability2016.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
@@ -110,7 +120,7 @@ namespace ProfitWise.Test
             Screenshot ss = ((ITakesScreenshot)driverGC).GetScreenshot();
             ss.SaveAsFile("c:\\Screenshots\\DrillDown.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 
-        }    
+        }
 
         [TestMethod]
         public void SetCustomDateRange()
@@ -122,53 +132,64 @@ namespace ProfitWise.Test
             wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
 
             //Wait for Date Range Picker to be available
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"report-date-picker\"]/div/div/input")));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".date-ranger-picker.form-control")));
 
-            driverGC.FindElement(By.XPath("//*[@id=\"report-date-picker\"]/div/div/input")).Click();
+            driverGC.FindElement(By.CssSelector(".date-ranger-picker.form-control")).Click();
 
             //Wait for Date Range Picker Input Fields to be available
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"report-date-picker\"]/div/div/div/div[1]/div[1]/input")));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("daterangepicker_start")));
 
-            driverGC.FindElement(By.XPath("//*[@id=\"report-date-picker\"]/div/div/div/div[1]/div[1]/input")).Clear();
-            driverGC.FindElement(By.XPath("//*[@id=\"report-date-picker\"]/div/div/div/div[1]/div[1]/input")).SendKeys("12\\1\\2016");
+            driverGC.FindElement(By.Name("daterangepicker_start")).Clear();
+            driverGC.FindElement(By.Name("daterangepicker_start")).SendKeys("12\\1\\2016");
 
-            driverGC.FindElement(By.XPath("//*[@id=\"report-date-picker\"]/div/div/div/div[2]/div[1]/input")).Clear();
-            driverGC.FindElement(By.XPath("//*[@id=\"report-date-picker\"]/div/div/div/div[2]/div[1]/input")).SendKeys("12\\31\\2016");
+            driverGC.FindElement(By.Name("daterangepicker_end")).Clear();
+            driverGC.FindElement(By.Name("daterangepicker_end")).SendKeys("12\\31\\2016");
 
-            driverGC.FindElement(By.XPath("//*[@id=\"report-date-picker\"]/div/div/div/div[3]/div/button[1]")).Click();
+            driverGC.FindElement(By.CssSelector(".applyBtn.btn.btn-sm.btn-primary")).Click();
 
-            
+
             //Wait for Spinner Layer to go away
             wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
             Thread.Sleep(2500);
+
+            //Check December, 2016 Revenues
+            var Dec2016Revenues = driverGC.FindElement(By.XPath("//*[@id=\"report-viewer-body\"]/div/div/div/div[2]/div/div[1]/div[2]/h1")).Text;
+            Assert.AreEqual("$296,613.65", Dec2016Revenues);
+            Console.WriteLine("Revenues for December, 2016 = " + Dec2016Revenues);
+
+            //Check December, 2016 Profits
+            var Dec2016Profit = driverGC.FindElement(By.XPath("//*[@id=\"report-viewer-body\"]/div/div/div/div[2]/div/div[1]/div[4]/h1")).Text;
+            Assert.AreEqual("$63,400.84", Dec2016Profit);
+            Console.WriteLine("Profit for December, 2016 = " + Dec2016Profit);
 
             Screenshot ss = ((ITakesScreenshot)driverGC).GetScreenshot();
             ss.SaveAsFile("c:\\Screenshots\\CustomDateRange.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 
         }
 
-        [TestMethod]
-        public void GroupByVendor()
-        {
-            WebDriverWait wait = new WebDriverWait(driverGC, TimeSpan.FromSeconds(30));
+        //[TestMethod]
+        //public void GroupByVendor()
+        //{
+        //    WebDriverWait wait = new WebDriverWait(driverGC, TimeSpan.FromSeconds(30));
 
-            //Wait for Spinner Layer to go away
-            wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
+        //    //Wait for Spinner Layer to go away
+        //    wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
 
-            //Wait for XXX to be available
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("XXX")));
+        //    //Wait for Group By drop-down to be available
+        //    wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"report-viewer-body\"]/div/div/div/div[2]/div/div[3]/div/div[1]/span[2]/span/a/span")));
 
-            driverGC.FindElement(By.XPath("XXX")).Click();
+        //    driverGC.FindElement(By.XPath("//*[@id=\"report-viewer-body\"]/div/div/div/div[2]/div/div[3]/div/div[1]/span[2]/span/a/span")).Click();
+        //    driverGC.FindElement(By.ClassName("form-control")).Click();
+        //    driverGC.FindElement(By.PartialLinkText("Vendor")).Click();
 
-            //Wait for Spinner Layer to go away
-            wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
-            Thread.Sleep(2000);
+        //    //Wait for Spinner Layer to go away
+        //    wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
+        //    Thread.Sleep(2000);
 
-            Screenshot ss = ((ITakesScreenshot)driverGC).GetScreenshot();
-            ss.SaveAsFile("c:\\Screenshots\\XXX.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        //    Screenshot ss = ((ITakesScreenshot)driverGC).GetScreenshot();
+        //    ss.SaveAsFile("c:\\Screenshots\\GroupByVendor.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 
-        }
-        //*[@id=\"report-viewer-body\"]/div/div/div/div[2]/div/div[3]/div/div[1]/span[2]/span/a/span
+        //}
 
         [TestMethod]
         public void LoadEditFilters()
@@ -186,11 +207,21 @@ namespace ProfitWise.Test
             //Wait for Spinner Layer to go away
             wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
 
+            //Add filter for 3D Printers
+            driverGC.FindElement(By.XPath("//*[@id=\"product-types\"]/tbody/tr[3]")).Click();
+            Thread.Sleep(1000);
+
+            //Check to make sure Number of Variants = 20
+            var NumVariants = driverGC.FindElement(By.XPath("//*[@id=\"product-variant-count-container\"]/div[3]/a/span")).Text;
+            Assert.AreEqual("20", NumVariants);
+            Console.WriteLine("Number of 3D Printer Variants = " + NumVariants);
+            
+
             Screenshot ss = ((ITakesScreenshot)driverGC).GetScreenshot();
             ss.SaveAsFile("c:\\Screenshots\\EditFilters.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 
             //Click OK to go back to Dashboard
-            driverGC.FindElement(By.XPath("//*[@id=\"report-editor-header-top\"]/div/div/div[2]/div/a")).Click();
+            driverGC.FindElement(By.CssSelector(".btn.btn-primary")).Click();
 
 
 
@@ -234,19 +265,64 @@ namespace ProfitWise.Test
             wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id=\"next-popover-activator--1\"]")));
 
             driverGC.FindElement(By.XPath("//*[@id=\"next-popover-activator--1\"]")).Click();
-            driverGC.FindElement(By.XPath("//*[@id=\"next-popover--1\"]/div[2]/div/div/ul/li[5]/button")).Click();
             Thread.Sleep(1000);
+            driverGC.FindElement(By.XPath("//*[@id=\"next-popover--1\"]/div[2]/div/div/ul/li[5]/button")).Click();
+            Thread.Sleep(2000);
             driverGC.SwitchTo().Frame("app-iframe");
             //Wait for Spinner Layer to go away
             wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
-            Thread.Sleep(2000);
 
             Screenshot ss = ((ITakesScreenshot)driverGC).GetScreenshot();
             ss.SaveAsFile("c:\\Screenshots\\EditCoGS.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
 
         }
 
+        [TestMethod]
+        public void EditCoGSValue()
+        {
+            WebDriverWait wait = new WebDriverWait(driverGC, TimeSpan.FromSeconds(30));
+          
+            //Wait for Search Field to be available
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.TagName("input")));
 
+            //Search for "Ultimaker 3"
+            driverGC.FindElement(By.TagName("input")).Click();
+            driverGC.FindElement(By.TagName("input")).SendKeys("Ultimaker 3" + Keys.Enter);
+
+            //Wait for Spinner Layer to go away
+            wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("spinner-layer")));
+            Thread.Sleep(1000);
+
+            //Click on the CoGS value for Ultimaker 3
+            driverGC.FindElement(By.XPath("//*[@id=\"product-search-grid\"]/tbody/tr[1]/td[5]/a/span/span[1]/span")).Click();
+
+            //Switch to the CoGS Editor popup
+            Thread.Sleep(1000);
+            driverGC.SwitchTo().DefaultContent();
+            driverGC.SwitchTo().Frame("app-modal-iframe");
+
+            //Enter new value for CoGS
+            Thread.Sleep(1000);
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".form-control.money-editor-text")));
+            //driverGC.FindElement(By.CssSelector(".form-control.money-editor-text")).Clear();
+            driverGC.FindElement(By.CssSelector(".form-control.money-editor-text")).SendKeys("2796.01" + Keys.Enter);
+
+            //Switch back to the main app window
+            Thread.Sleep(1000);
+            driverGC.SwitchTo().DefaultContent();
+            driverGC.SwitchTo().Frame("app-iframe");
+
+            var CoGSValue = driverGC.FindElement(By.XPath("//*[@id=\"product-search-grid\"]/tbody/tr[1]/td[5]/a/span/span[1]/span")).Text;
+            Assert.AreEqual(CoGSValue, "$2,796.01 USD");
+            Console.WriteLine("CoGS for Ultimaker 3 = " + CoGSValue);
+
+
+
+            //Take a screenshot
+            Screenshot ss = ((ITakesScreenshot)driverGC).GetScreenshot();
+            ss.SaveAsFile("c:\\Screenshots\\EditCoGSValue.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+        }
 
 
         //[TestMethod]
@@ -276,7 +352,7 @@ namespace ProfitWise.Test
         public static void Cleanup()
         {
 
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
             driverGC.Quit();
         }
 
