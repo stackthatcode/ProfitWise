398c21669257ec8b6e199311cad66a3a24a0a01e
Aleks Jones <ajones@3duniverse.org>
2017-01-13 10:07:45 -0600

    More code cleaning chores to make Number Editor reusable

--- ProfitWise.Web/Scripts/ProfitWiseCurrency.js
+++ ProfitWise.Web/Scripts/ProfitWiseCurrency.js
@@ -33,10 +33,39 @@ ProfitWiseFunctions.FormatCurrency = function (amount, currencyId) {
     return item.Symbol + numeral(amount).format("0,0.00");
 };
 
-ProfitWiseFunctions.UnformatCurrency = function (amount) {
+ProfitWiseFunctions.ExtractRawNumber = function (amount) {
     amount = amount || 0;
     amountAsString = typeof amount == "string" ? amount : amount.toString();
     return numeral().unformat(amountAsString);
 };
+
+ProfitWiseFunctions.MakeKnockoutNumberOnlyInterceptor
+    = function (knockoutValue, lowConstraint, highConstraint) {
+        return ko.computed({
+            read: function() {
+                // Here's the formatting injection point
+                var numericValue = ProfitWiseFunctions.ExtractRawNumber(ko.unwrap(knockoutValue));
+                numericValue =
+                    (lowConstraint && numericValue < lowConstraint)
+                        ? lowConstraint
+                        : numericValue;
+                numericValue =
+                    (highConstraint && numericValue > highConstraint)
+                        ? highConstraint
+                        : numericValue;
+
+                var output = numeral(numericValue).format('0,0.00');
+                return output;
+            },
+            write: function(newValue) {
+                if ($.trim(newValue) == '') {
+                    knockoutValue("0");
+                } else {
+                    knockoutValue(numeral().unformat(newValue));
+                }
+                knockoutValue.valueHasMutated();
+            }
+        }).extend({ notify: 'always' });
+    };
 
 
