7a9b9b64af4b85262480a1ce6e7c911cc7aa9762
Jeremy Simon <jeremy@3duniverse.org>
2017-02-01 18:37:53 -0600

    Added some stuff to Step # 2 of Loader.cs

--- ProfitWise.Test/OrderLoader/Loader.cs
+++ ProfitWise.Test/OrderLoader/Loader.cs
@@ -5,6 +5,7 @@ using Push.Shopify.Factories;
 using Push.Shopify.HttpClient;
 using Push.Shopify.Interfaces;
 using Push.Shopify.Model;
+using System;
 
 namespace ProfitWise.Test.OrderLoader
 {
@@ -16,7 +17,8 @@ namespace ProfitWise.Test.OrderLoader
         {
             var container = ProfitWise.Batch.AutofacRegistration.Build();
 
-            var userId = "7ab46e72-3b1c-4db1-88d4-f8a6b8f3e57a";
+            // Aleks: var userId = "7ab46e72-3b1c-4db1-88d4-f8a6b8f3e57a";
+            var userId = "c5223b47-aa25-4261-9639-68d749bdc38b";
 
             using (var scope = container.BeginLifetimeScope())
             {
@@ -36,13 +38,93 @@ namespace ProfitWise.Test.OrderLoader
 
                 var filter = new ProductFilter();
                 var products = productApiRepository.Retrieve(filter);
+                var numProducts = products.Count;
 
                 // Step #2 - prepare a plan to upload random Orders
 
-                
+
+                //Create 5000 orders
+                for (int i = 1; i < 5000; i = i + 1)
+                {
+
+                    var randomDate = RandomDay();
+
+                    Random rnd = new Random();
+                    //Set number of line items to random # 1-10
+                    int numLineItems = rnd.Next(1, 10);
+
+                    //create 1-10 line items
+                    for (int li = 1; li < numLineItems; li = li + 1)
+                    {
+                        //Set line item quantity to random # 1-10
+                        int quantity = rnd.Next(1, 10);
+
+                        //Pick random product index
+                        var productNum = rnd.Next(0, numProducts);
+
+                        //Pick random variant index from selected product
+                        var variantNum = rnd.Next(0, products[productNum].Variants.Count);
+
+                        //Set VariantId to be inserted into order line item
+                        var variantId = products[productNum].Variants[variantNum].Id;
+
+                        
+
+                    }
+
+                }
+
 
                 // Step #3 - Insert Orders into Shopify API
+
+                //This is a sample of what we need to generate:
+                //
+                //POST / admin / orders.json
+                //{
+                //  "order": {
+                //             "line_items": [
+                //                          {
+                //                            "title": "Product title",
+                //                            "price": 74.99,
+                //                            "grams": "1300",
+                //                            "quantity": 3,
+                //                            "tax_lines": [
+                //                              {
+                //                                "price": 13.5,
+                //                                "rate": 0.06,
+                //                                "title": "State tax"
+                //                              }
+                //                                         ]
+                //                          }
+                //                            ],
+                //    "transactions": [
+                //      {
+                //		  "test": "true",
+                //        "kind": "sale",
+                //        "status": "success",
+                //        "amount": 238.47
+                //      }
+                //                     ],
+                //    "total_tax": 13.5,
+                //    "currency": "USD"
+                //   }
+                //}
+
+
+
+
             }
+
+       }
+
+        private Random gen = new Random();
+        DateTime RandomDay()
+        {
+            DateTime start = new DateTime(2014, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range));
         }
+
     }
+
 }
