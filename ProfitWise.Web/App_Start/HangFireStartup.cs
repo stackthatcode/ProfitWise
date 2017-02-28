b69de0168c7fe80a830291208720bf1215c1fd2d
Aleks Jones <ajones@3duniverse.org>
2017-02-28 14:10:05 -0600

    Added back link to Admin Dashboard

--- ProfitWise.Web/App_Start/HangFireStartup.cs
+++ ProfitWise.Web/App_Start/HangFireStartup.cs
@@ -1,6 +1,7 @@
 ﻿using Hangfire;
 using Owin;
 using ProfitWise.Web.Attributes;
+using ProfitWise.Web.Plumbing;
 
 namespace ProfitWise.Web
 {
@@ -13,7 +14,8 @@ namespace ProfitWise.Web
             app.UseHangfireDashboard(
                 "/hangfire", new DashboardOptions
                 {
-                    Authorization = new[] { new HangFireAuthorizationFilter(),  }
+                    Authorization = new[] { new HangFireAuthorizationFilter(),  },
+                    AppPath = GlobalConfig.BaseUrl + "/AdminHome/Index"
                 });
         }
     }
