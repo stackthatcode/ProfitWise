211bac1a7b5f7845e1450d34deba600091b572b8
Aleks Jones <ajones@3duniverse.org>
2017-07-06 15:41:45 -0500

    PW-182 => please execute script to fix bad data

---
+++ SQL/SqlServer/Release v1.0/Deployed/PW-156.sql
@@ -0,0 +1,35 @@
+USE ProfitWise
+GO
+
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'profitwiseprofitreportentry' AND COLUMN_NAME = 'OrderCountOrderId')
+BEGIN
+    ALTER TABLE [dbo].[profitwiseprofitreportentry] ADD 
+        [OrderCountOrderId] bigint NULL DEFAULT NULL
+		
+END
+
+
+-- *** CRITICAL => the Functions and Views script must be executed before the next script is run before this
+
+
+
+DROP FUNCTION IF EXISTS dbo.profitreportentryprocessed
+GO
+CREATE FUNCTION dbo.profitreportentryprocessed (
+		@PwShopId bigint, @UseDefaultMargin tinyint, @DefaultCogsPercent decimal(15, 2), @MinPaymentStatus tinyint)  
+RETURNS TABLE
+AS  
+RETURN SELECT PwShopId, EntryDate, EntryType, ShopifyOrderId, SourceId, PwProductId, PwVariantId, NetSales, 
+	CASE WHEN (@UseDefaultMargin = 1 AND ISNULL(CoGS, 0) = 0) THEN NetSales * @DefaultCoGSPercent ELSE CoGS END AS CoGS, 
+	Quantity, PaymentStatus, OrderCountOrderId
+FROM dbo.profitreportentry(@PwShopId)
+WHERE PaymentStatus >= @MinPaymentStatus
+GO
+
+
+
+-- #### IMPORTANT - DON'T FORGET TO RUN THE VIEWS AND FUNCTIONS DDL ####
+
+
+
---
+++ SQL/SqlServer/Release v1.0/PW-182.sql
@@ -0,0 +1,5 @@
+USE ProfitWise
+GO
+
+UPDATE variant(100001) SET IsPrimary = 1 WHERE PwProductId = 199 AND PwVariantId = 908;
+
