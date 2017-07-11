a20842e7f6ca5bb4b6065e3ec0f724513670e4dc
Aleks Jones <ajones@3duniverse.org>
2017-07-11 17:40:30 -0500

    Eeeee - patched bad hack script

--- SQL/SqlServer/Tools/QuickEraseForShop  - NEVER RUN ON PROD.sql
+++ SQL/SqlServer/Tools/QuickEraseForShop  - NEVER RUN ON PROD.sql
@@ -4,12 +4,24 @@ GO
 
 -- This script will rename a Shop's Domain in ASP.NET Users, thereby preventing PW from verifying their existence
 
-DECLARE @PwShopId int = 999999;
+DECLARE @PwShopId int = 100001;
 
 UPDATE t1
 SET UserName = UserName + 'DISABLED'
 FROM AspNetUsers t1
 	INNER JOIN shop(@PwShopId) t2
 		ON t1.Id = t2.ShopOwnerUserId;
+
+DELETE FROM AspNetUserClaims WHERE UserId IN 
+	( SELECT Id FROM AspNetUsers t1
+		INNER JOIN shop(@PwShopId) t2
+			ON t1.Id = t2.ShopOwnerUserId );
+
+DELETE FROM AspNetUserLogins WHERE UserId IN 
+	( SELECT Id FROM AspNetUsers t1
+		INNER JOIN shop(@PwShopId) t2
+			ON t1.Id = t2.ShopOwnerUserId );
+
+
 
 
